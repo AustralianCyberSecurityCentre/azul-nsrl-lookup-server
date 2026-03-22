ae0e54e1edea8f9c9e3b74adcf569173f6da1468
dan-acsc <dan-acsc@users.noreply.github.com>
2026-03-22 23:55:58 +0000

    feat: upgrade sql alchemy to 2+

--- azul_nsrl_lookup_server/database.py
+++ azul_nsrl_lookup_server/database.py
@@ -1,8 +1,8 @@
 """Database connection setup."""
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import DeferredReflection, declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import DeferredReflection
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from . import settings
 
@@ -14,7 +14,6 @@ def setup_engine():
     """Delay setting up the engine until called."""
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
     return engine, SessionLocal
 
 
